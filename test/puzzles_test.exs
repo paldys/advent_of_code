91e7a6b97fa218b76f951821cd435c47b43ee9ea
Alex Paldy <paldys@yahoo.com>
2021-12-01 22:20:53 +0000

    Refactor tests, run them from single module

--- test/puzzles_test.exs
+++ test/puzzles_test.exs
@@ -1,7 +1,7 @@
-defmodule AdventOfCode.Puzzles.Day011Test do
+defmodule AdventOfCode.PuzzlesTest do
   use ExUnit.Case
 
-  test "solve example" do
+  test "day 01-1 puzzle" do
     measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
     assert AdventOfCode.Puzzles.Day011.solve(measurements) == 7
   end

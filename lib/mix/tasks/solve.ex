9b26978e25c5812e559fb40f25a66096a2c63619
Alex Paldy <paldys@yahoo.com>
2021-12-01 20:51:17 +0000

    Solve Day 1 first puzzle

---
+++ lib/mix/tasks/solve.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.Solve do
+  use Mix.Task
+
+  @shortdoc "Solve the puzzle"
+  def run(_) do
+    File.read!("resources/01-1.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> AdventOfCode.Puzzles.Day011.solve()
+    |> IO.puts()
+  end
+end

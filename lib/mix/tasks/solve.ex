890426821ae7cb01156b0dbcb713632c61059ba7
Alex Paldy <paldys@yahoo.com>
2021-12-02 18:57:15 +0000

    Solve Day 2 first puzzle

--- lib/mix/tasks/solve.ex
+++ lib/mix/tasks/solve.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Solve do
   use Mix.Task
 
-  alias AdventOfCode.Puzzles.Day012, as: Puzzle
+  alias AdventOfCode.Puzzles.Day021, as: Puzzle
 
   @shortdoc "Solve the puzzle"
   def run(_) do

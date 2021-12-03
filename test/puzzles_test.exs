afd06407631dd523df872cdcc013fa2e5cf88ed7
Alex Paldy <paldys@yahoo.com>
2021-12-03 19:36:46 +0000

    [Refactor] Use constants in tests for inputs

--- test/puzzles_test.exs
+++ test/puzzles_test.exs
@@ -1,61 +1,46 @@
 defmodule AdventOfCode.PuzzlesTest do
   use ExUnit.Case
 
+  @day_01_test_input [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
   test "day 01-1 puzzle" do
-    measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    assert AdventOfCode.Puzzles.Day011.solve(measurements) == 7
+    assert AdventOfCode.Puzzles.Day011.solve(@day_01_test_input) == 7
   end
 
   test "day 01-2 puzzle" do
-    measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    assert AdventOfCode.Puzzles.Day012.solve(measurements) == 5
+    assert AdventOfCode.Puzzles.Day012.solve(@day_01_test_input) == 5
   end
 
+  @day_02_test_input [{:forward, 5}, {:down, 5}, {:forward, 8}, {:up, 3}, {:down, 8}, {:forward, 2}]
+
   test "day 02-1 puzzle" do
-    commands = [{:forward, 5}, {:down, 5}, {:forward, 8}, {:up, 3}, {:down, 8}, {:forward, 2}]
-    assert AdventOfCode.Puzzles.Day021.solve(commands) == 150
+    assert AdventOfCode.Puzzles.Day021.solve(@day_02_test_input) == 150
   end
 
   test "day 02-2 puzzle" do
-    commands = [{:forward, 5}, {:down, 5}, {:forward, 8}, {:up, 3}, {:down, 8}, {:forward, 2}]
-    assert AdventOfCode.Puzzles.Day022.solve(commands) == 900
+    assert AdventOfCode.Puzzles.Day022.solve(@day_02_test_input) == 900
   end
 
-  test "day 03-1 puzzle" do
-    codes = [
-      '00100',
-      '11110',
-      '10110',
-      '10111',
-      '10101',
-      '01111',
-      '00111',
-      '11100',
-      '10000',
-      '11001',
-      '00010',
-      '01010'
-    ]
+  @day_03_test_input [
+    '00100',
+    '11110',
+    '10110',
+    '10111',
+    '10101',
+    '01111',
+    '00111',
+    '11100',
+    '10000',
+    '11001',
+    '00010',
+    '01010'
+  ]
 
-    assert AdventOfCode.Puzzles.Day031.solve(codes) == 198
+  test "day 03-1 puzzle" do
+    assert AdventOfCode.Puzzles.Day031.solve(@day_03_test_input) == 198
   end
 
   test "day 03-2 puzzle" do
-    codes = [
-      '00100',
-      '11110',
-      '10110',
-      '10111',
-      '10101',
-      '01111',
-      '00111',
-      '11100',
-      '10000',
-      '11001',
-      '00010',
-      '01010'
-    ]
-
-    assert AdventOfCode.Puzzles.Day032.solve(codes) == 230
+    assert AdventOfCode.Puzzles.Day032.solve(@day_03_test_input) == 230
   end
 end

063f3a0c4551b8dc28674b3e4fe38becb856b9e7
Alex Paldy <paldys@yahoo.com>
2021-12-03 18:25:12 +0000

    Solve Day 3 first puzzle

--- test/puzzles_test.exs
+++ test/puzzles_test.exs
@@ -20,4 +20,9 @@ defmodule AdventOfCode.PuzzlesTest do
     commands = [{:forward, 5}, {:down, 5}, {:forward, 8}, {:up, 3}, {:down, 8}, {:forward, 2}]
     assert AdventOfCode.Puzzles.Day022.solve(commands) == 900
   end
+
+  test "day 03-1 puzzle" do
+    codes = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
+    assert AdventOfCode.Puzzles.Day031.solve(codes) == 198
+  end
 end

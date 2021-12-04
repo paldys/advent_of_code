86b79618d068a8c50ea54341e873f11e724e64d8
Alex Paldy <paldys@yahoo.com>
2021-12-04 23:18:37 +0000

    Dynamically load solvers from arguments

--- lib/mix/tasks/solve.ex
+++ lib/mix/tasks/solve.ex
@@ -1,12 +1,20 @@
 defmodule Mix.Tasks.Solve do
   use Mix.Task
 
-  alias AdventOfCode.Puzzles.Day042, as: Puzzle
-
   @shortdoc "Solve the puzzle"
-  def run(_) do
-    Puzzle.load()
-    |> Puzzle.solve()
+  def run(args) do
+    puzzle_solver_module = puzzle_solver_module(args)
+    Code.ensure_loaded!(puzzle_solver_module)
+
+    puzzle_input = apply(puzzle_solver_module, :load, [])
+
+    apply(puzzle_solver_module, :solve, [puzzle_input])
     |> IO.puts()
+  end
+
+  defp puzzle_solver_module([day, puzzle]) do
+    String.to_existing_atom(
+      "Elixir.AdventOfCode.Puzzles.Day#{String.pad_leading(day, 2, "0")}#{puzzle}"
+    )
   end
 end

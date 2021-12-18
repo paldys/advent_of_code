4a2b4c44b8f79f7a4790f13804429ff7c769bc76
Alex Paldy <paldys@yahoo.com>
2021-12-18 01:22:37 +0100

    Solve Day 16 second puzzle

--- lib/puzzles/day_16.ex
+++ lib/puzzles/day_16.ex
@@ -154,8 +154,64 @@ defmodule AdventOfCode.Puzzles.Day16 do
 
   Decode the structure of your hexadecimal-encoded BITS transmission; what do
   you get if you add up the version numbers in all packets?
+
+  --- Part Two ---
+
+  Now that you have the structure of your transmission decoded, you can calculate the value of the expression it represents.
+
+  Literal values (type ID 4) represent a single number as described above. The remaining type IDs are more interesting:
+
+      - Packets with type ID 0 are sum packets - their value is the sum of
+      the values of their sub-packets. If they only have a single sub-packet,
+      their value is the value of the sub-packet.
+      - Packets with type ID 1 are product packets - their value is the result
+      of multiplying together the values of their sub-packets. If they only have
+      a single sub-packet, their value is the value of the sub-packet.
+      - Packets with type ID 2 are minimum packets - their value is the minimum
+      of the values of their sub-packets.
+      - Packets with type ID 3 are maximum packets - their value is the maximum
+      of the values of their sub-packets.
+      - Packets with type ID 5 are greater than packets - their value is 1 if
+      the value of the first sub-packet is greater than the value of
+      the second sub-packet; otherwise, their value is 0. These packets always
+       have exactly two sub-packets.
+      - Packets with type ID 6 are less than packets - their value is 1 if
+      the value of the first sub-packet is less than the value of the second
+      sub-packet; otherwise, their value is 0. These packets always have exactly
+      two sub-packets.
+      - Packets with type ID 7 are equal to packets - their value is 1 if
+      the value of the first sub-packet is equal to the value of the second
+      sub-packet; otherwise, their value is 0. These packets always have exactly
+      two sub-packets.
+
+  Using these rules, you can now work out the value of the outermost packet
+  in your BITS transmission.
+
+  For example:
+
+    - C200B40A82 finds the sum of 1 and 2, resulting in the value 3.
+    - 04005AC33890 finds the product of 6 and 9, resulting in the value 54.
+    - 880086C3E88112 finds the minimum of 7, 8, and 9, resulting in the value 7.
+    - CE00C43D881120 finds the maximum of 7, 8, and 9, resulting in the value 9.
+    - D8005AC2A8F0 produces 1, because 5 is less than 15.
+    - F600BC2D8F produces 0, because 5 is not greater than 15.
+    - 9C005AC2F8F0 produces 0, because 5 is not equal to 15.
+    - 9C0141080250320F1802104A08 produces 1, because 1 + 3 = 2 * 2.
+
+  What do you get if you evaluate the expression represented by your
+  hexadecimal-encoded BITS transmission?
   """
   use Bitwise, only_operators: true
+
+  @operators %{
+    0 => :sum,
+    1 => :product,
+    2 => :minimum,
+    3 => :maximum,
+    5 => :greater_than,
+    6 => :less_than,
+    7 => :equal_to
+  }
 
   def load() do
     File.read!("resources/day-16-input.txt")
@@ -168,8 +224,24 @@ defmodule AdventOfCode.Puzzles.Day16 do
     version_sum(decoded_packet)
   end
 
-  def solve2(_) do
-    nil
+  def solve2(transmission) do
+    transmission_bits = Base.decode16!(transmission)
+    {decoded_packet, _} = decode_packet(transmission_bits)
+
+    evaluate(decoded_packet)
+  end
+
+  defp evaluate(packet) do
+    case packet do
+      {:sum, _, subs} -> Enum.map(subs, &evaluate/1) |> Enum.sum()
+      {:product, _, subs} -> Enum.map(subs, &evaluate/1) |> Enum.product()
+      {:minimum, _, subs} -> Enum.map(subs, &evaluate/1) |> Enum.min()
+      {:maximum, _, subs} -> Enum.map(subs, &evaluate/1) |> Enum.max()
+      {:literal, _, value} -> value
+      {:greater_than, _, [sub1, sub2]} -> if evaluate(sub1) < evaluate(sub2), do: 1, else: 0
+      {:less_than, _, [sub1, sub2]} -> if evaluate(sub1) > evaluate(sub2), do: 1, else: 0
+      {:equal_to, _, [sub1, sub2]} -> if evaluate(sub1) == evaluate(sub2), do: 1, else: 0
+    end
   end
 
   defp version_sum(packet, versions \\ 0)
@@ -178,7 +250,7 @@ defmodule AdventOfCode.Puzzles.Day16 do
     versions + version
   end
 
-  defp version_sum({:operator, version, subs}, versions) do
+  defp version_sum({_, version, subs}, versions) do
     sub_versions =
       Enum.map(subs, fn sub -> version_sum(sub, 0) end)
       |> Enum.sum()
@@ -194,7 +266,8 @@ defmodule AdventOfCode.Puzzles.Day16 do
 
       _ ->
         {subpackets, rest} = decode_operator(rest)
-        {{:operator, version, subpackets}, rest}
+        operator = Map.get(@operators, type)
+        {{operator, version, subpackets}, rest}
     end
   end
 
--- test/puzzles/day_16_test.exs
+++ test/puzzles/day_16_test.exs
@@ -10,7 +10,7 @@ defmodule AdventOfCode.Puzzles.Day16Test do
     {decoded_packet, _} =
       AdventOfCode.Puzzles.Day16.decode_packet(Base.decode16!("38006F45291200"))
 
-    assert decoded_packet == {:operator, 1, [{:literal, 2, 20}, {:literal, 6, 10}]}
+    assert decoded_packet == {:less_than, 1, [{:literal, 2, 20}, {:literal, 6, 10}]}
   end
 
   test "decode operator with count" do
@@ -18,7 +18,7 @@ defmodule AdventOfCode.Puzzles.Day16Test do
       AdventOfCode.Puzzles.Day16.decode_packet(Base.decode16!("EE00D40C823060"))
 
     assert decoded_packet ==
-             {:operator, 7, [{:literal, 1, 3}, {:literal, 4, 2}, {:literal, 2, 1}]}
+             {:maximum, 7, [{:literal, 1, 3}, {:literal, 4, 2}, {:literal, 2, 1}]}
   end
 
   test "solve 1st puzzle" do
@@ -29,6 +29,13 @@ defmodule AdventOfCode.Puzzles.Day16Test do
   end
 
   test "solve 2nd puzzle" do
-    assert AdventOfCode.Puzzles.Day16.solve2(nil) == nil
+    assert AdventOfCode.Puzzles.Day16.solve2("C200B40A82") == 3
+    assert AdventOfCode.Puzzles.Day16.solve2("04005AC33890") == 54
+    assert AdventOfCode.Puzzles.Day16.solve2("880086C3E88112") == 7
+    assert AdventOfCode.Puzzles.Day16.solve2("CE00C43D881120") == 9
+    assert AdventOfCode.Puzzles.Day16.solve2("D8005AC2A8F0") == 1
+    assert AdventOfCode.Puzzles.Day16.solve2("F600BC2D8F") == 0
+    assert AdventOfCode.Puzzles.Day16.solve2("9C005AC2F8F0") == 0
+    assert AdventOfCode.Puzzles.Day16.solve2("9C0141080250320F1802104A08") == 1
   end
 end

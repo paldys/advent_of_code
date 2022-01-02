e32c9b965dcfeb5670e8ccdc3772dbc578ea6146
Alex Paldy <paldys@yahoo.com>
2022-01-02 22:59:53 +0100

    Parse Day 25 input

---
+++ test/puzzles/day_25_test.exs
@@ -0,0 +1,39 @@
+defmodule AdventOfCode.Puzzles.Day25Test do
+  use ExUnit.Case
+
+  @initial_state Arrays.new([
+                   Arrays.new([?v, ?., ?., ?., ?>, ?>, ?., ?v, ?v, ?>]),
+                   Arrays.new([?., ?v, ?v, ?>, ?>, ?., ?v, ?v, ?., ?.]),
+                   Arrays.new([?>, ?>, ?., ?>, ?v, ?>, ?., ?., ?., ?v]),
+                   Arrays.new([?>, ?>, ?v, ?>, ?>, ?., ?>, ?., ?v, ?.]),
+                   Arrays.new([?v, ?>, ?v, ?., ?v, ?v, ?., ?v, ?., ?.]),
+                   Arrays.new([?>, ?., ?>, ?>, ?., ?., ?v, ?., ?., ?.]),
+                   Arrays.new([?., ?v, ?v, ?., ?., ?>, ?., ?>, ?v, ?.]),
+                   Arrays.new([?v, ?., ?v, ?., ?., ?>, ?>, ?v, ?., ?v]),
+                   Arrays.new([?., ?., ?., ?., ?v, ?., ?., ?v, ?., ?>])
+                 ])
+
+  test "parse input" do
+    test_input = """
+    v...>>.vv>
+    .vv>>.vv..
+    >>.>v>...v
+    >>v>>.>.v.
+    v>v.vv.v..
+    >.>>..v...
+    .vv..>.>v.
+    v.v..>>v.v
+    ....v..v.>
+    """
+
+    assert AdventOfCode.Puzzles.Day25.parse(test_input) == @initial_state
+  end
+
+  test "solve 1st puzzle" do
+    assert AdventOfCode.Puzzles.Day25.solve1(nil) == nil
+  end
+
+  test "solve 2nd puzzle" do
+    assert AdventOfCode.Puzzles.Day25.solve2(nil) == nil
+  end
+end

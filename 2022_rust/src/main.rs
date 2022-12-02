2573534297a776855b8c1d83c25e115050953a35
Alex Paldy <paldys@yahoo.com>
2022-12-02 00:47:24 +0000

    Solve Day 1 second puzzle

--- 2022_rust/src/main.rs
+++ 2022_rust/src/main.rs
@@ -12,8 +12,18 @@ struct Args {
 }
 
 fn main() {
+    let args = Args::parse();
+
+    println!("Solve Day 1 Puzzle {}", args.puzzle);
+
     let day_01_input =
         fs::read_to_string("resources/day_01_input.txt").expect("Could not read input");
 
-    println!("Answer: {}", puzzles::day_01::solve_first(day_01_input))
+    let answer = if args.puzzle == 1 {
+        puzzles::day_01::solve_first(day_01_input)
+    } else {
+        puzzles::day_01::solve_second(day_01_input)
+    };
+
+    println!("Answer: {}", answer)
 }
--- 2022_rust/src/puzzles/day_01.rs
+++ 2022_rust/src/puzzles/day_01.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 pub fn solve_first(input: String) -> u32 {
     let mut most_calories = 0;
     let mut current_calories = 0;
@@ -12,6 +14,29 @@ pub fn solve_first(input: String) -> u32 {
         }
     }
     most_calories.max(current_calories)
+}
+
+pub fn solve_second(input: String) -> u32 {
+    let mut calory_heap: BinaryHeap<u32> = BinaryHeap::new();
+
+    let mut current_calories = 0;
+    for line in input.split('\n') {
+        let trimmed_line = line.trim();
+        if trimmed_line.is_empty() {
+            calory_heap.push(current_calories);
+            current_calories = 0;
+        } else {
+            let calory: u32 = trimmed_line.parse().expect("Not a number");
+            current_calories += calory;
+        }
+    }
+    calory_heap.push(current_calories);
+
+    let mut most_calories_sum = 0;
+    for _ in 1..=3 {
+        most_calories_sum += calory_heap.pop().unwrap_or(0);
+    }
+    most_calories_sum
 }
 
 #[cfg(test)]
@@ -36,5 +61,10 @@ mod tests {
     #[test]
     fn solves_first() {
         assert_eq!(24000, solve_first(String::from(RAW_INPUT)))
+    }
+
+    #[test]
+    fn solves_second() {
+        assert_eq!(45000, solve_second(String::from(RAW_INPUT)))
     }
 }

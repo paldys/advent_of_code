49608657d66c66b52b59897fe34732d410b9e1aa
Alex Paldy <paldys@yahoo.com>
2022-12-02 23:46:25 +0000

    Handle multiple day's input

--- 2022_rust/src/puzzles.rs
+++ 2022_rust/src/puzzles.rs
@@ -1,1 +1,12 @@
-pub mod day_01;
+mod day_01;
+
+fn panic_not_implemented(_: String) -> u32 {
+    panic!("Not implemented")
+}
+
+pub fn get_all_puzzles() -> [(fn(String) -> u32, fn(String) -> u32); 2] {
+    [
+        (day_01::solve_first, day_01::solve_second),
+        (panic_not_implemented, panic_not_implemented),
+    ]
+}

4f6f8c5f8c080efab8e34d7bb057f9cdea642ea2
Alex Paldy <paldys@yahoo.com>
2022-12-03 11:00:32 +0000

    Fill in all the future puzzles

--- 2022_rust/src/puzzles.rs
+++ 2022_rust/src/puzzles.rs
@@ -6,10 +6,32 @@ fn panic_not_implemented(_: String) -> u32 {
     panic!("Not implemented")
 }
 
-pub fn get_all_puzzles() -> [(fn(String) -> u32, fn(String) -> u32); 3] {
+pub fn get_all_puzzles() -> [(fn(String) -> u32, fn(String) -> u32); 25] {
     [
         (day_01::solve_first, day_01::solve_second),
         (day_02::solve_first, day_02::solve_second),
         (day_03::solve_first, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
+        (panic_not_implemented, panic_not_implemented),
     ]
 }

ea585d3e577ac2370a7866fdf1da09df499bb5a5
Alex Paldy <paldys@yahoo.com>
2022-12-03 00:34:48 +0000

    Solve Day 2 Puzzle Part 2

--- 2022_rust/src/puzzles.rs
+++ 2022_rust/src/puzzles.rs
@@ -5,9 +5,10 @@ fn panic_not_implemented(_: String) -> u32 {
     panic!("Not implemented")
 }
 
-pub fn get_all_puzzles() -> [(fn(String) -> u32, fn(String) -> u32); 2] {
+pub fn get_all_puzzles() -> [(fn(String) -> u32, fn(String) -> u32); 3] {
     [
         (day_01::solve_first, day_01::solve_second),
-        (day_02::solve_first, panic_not_implemented),
+        (day_02::solve_first, day_02::solve_second),
+        (panic_not_implemented, panic_not_implemented),
     ]
 }

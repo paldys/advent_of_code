f92c6ab81a31b524e9eed2140a36946edfc7b482
Alex Paldy <paldys@yahoo.com>
2022-12-14 17:05:14 +0000

    Solve Day 13 Puzzle Part 1

--- 2022_rust/src/puzzles.rs
+++ 2022_rust/src/puzzles.rs
@@ -10,6 +10,7 @@ mod day_09;
 mod day_10;
 mod day_11;
 mod day_12;
+mod day_13;
 
 #[derive(Debug, PartialEq)]
 pub enum Result {
@@ -37,7 +38,7 @@ pub fn get_all_puzzles() -> [Puzzle; 25] {
         (day_10::solve_first, day_10::solve_second),
         (day_11::solve_first, day_11::solve_second),
         (day_12::solve_first, day_12::solve_second),
-        (panic_not_implemented, panic_not_implemented),
+        (day_13::solve_first, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),

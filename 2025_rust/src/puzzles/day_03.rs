8d4469f492fb1d2946bcf5c9b61e9d74586d4582
Alex Paldy <paldys@yahoo.com>
2025-12-14 00:20:49 +0000

    Solve Puzzle 3 Part 2

--- 2025_rust/src/puzzles/day_03.rs
+++ 2025_rust/src/puzzles/day_03.rs
@@ -4,32 +4,41 @@ const MIN_DIGIT: u8 = b'0';
 const MAX_DIGIT: u8 = b'9';
 
 pub fn solve_first(input: String) -> Result {
-    let joltage = input.trim().lines().map(calculate_joltage).sum();
+    solve(input, 2)
+}
+
+pub fn solve_second(input: String) -> Result {
+    solve(input, 12)
+}
+
+fn solve(input: String, size: usize) -> Result {
+    let joltage = input
+        .lines()
+        .map(|line| calculate_joltage(line, size))
+        .sum();
     Result::Number(joltage)
 }
 
-fn calculate_joltage(line: &str) -> u64 {
+fn calculate_joltage(line: &str, size: usize) -> u64 {
     let bytes = line.as_bytes();
-    let mut i = 0;
-    let mut start_i = 0;
-    let mut start_value = MIN_DIGIT;
-    while i < bytes.len() - 1 && start_value != MAX_DIGIT {
-        if bytes[i] > start_value {
-            start_value = bytes[i];
-            start_i = i;
+    let mut joltage = 0_u64;
+    let mut i;
+    let mut max;
+    let mut next_start = 0;
+    for d in (0..=size - 1).rev() {
+        i = next_start;
+        max = MIN_DIGIT;
+        while i < bytes.len() - d && max != MAX_DIGIT {
+            if bytes[i] > max {
+                max = bytes[i];
+                next_start = i + 1;
+            }
+            i += 1;
         }
-        i += 1;
+        joltage = joltage * 10 + u64::from(max - MIN_DIGIT);
     }
 
-    i = bytes.len() - 1;
-    let mut end_value = MIN_DIGIT;
-    while i > start_i && end_value != MAX_DIGIT {
-        if bytes[i] > end_value {
-            end_value = bytes[i];
-        }
-        i -= 1;
-    }
-    10 * u64::from(start_value - MIN_DIGIT) + u64::from(end_value - MIN_DIGIT)
+    joltage
 }
 
 #[cfg(test)]
@@ -47,5 +56,10 @@ mod tests {
     #[test]
     fn solves_first() {
         assert_eq_number(357, solve_first(String::from(RAW_INPUT)));
+    }
+
+    #[test]
+    fn solves_second() {
+        assert_eq_number(3121910778619, solve_second(String::from(RAW_INPUT)));
     }
 }

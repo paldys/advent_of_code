a384c5b74c7fe3667a3faa6b38824dabb98a5b2b
Alex Paldy <paldys@yahoo.com>
2025-12-14 01:11:21 +0000

    Solve Puzzle 4 part 1

--- 2025_rust/src/puzzles.rs
+++ 2025_rust/src/puzzles.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
 #[derive(Debug, PartialEq)]
 pub enum Result {
@@ -19,7 +20,7 @@ pub fn get_all_puzzles() -> [Puzzle; 12] {
         (day_01::solve_first, day_01::solve_second),
         (day_02::solve_first, day_02::solve_second),
         (day_03::solve_first, day_03::solve_second),
-        (panic_not_implemented, panic_not_implemented),
+        (day_04::solve_first, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),

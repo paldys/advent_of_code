2e1e1ff066ac0bc59ecca9f62a9258c668438887
Alex Paldy <paldys@yahoo.com>
2025-12-15 21:48:58 +0000

    Solve Puzzle 5 part 2

--- 2025_rust/src/puzzles.rs
+++ 2025_rust/src/puzzles.rs
@@ -22,7 +22,7 @@ pub fn get_all_puzzles() -> [Puzzle; 12] {
         (day_02::solve_first, day_02::solve_second),
         (day_03::solve_first, day_03::solve_second),
         (day_04::solve_first, day_04::solve_second),
-        (day_05::solve_first, panic_not_implemented),
+        (day_05::solve_first, day_05::solve_second),
         (panic_not_implemented, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),
         (panic_not_implemented, panic_not_implemented),

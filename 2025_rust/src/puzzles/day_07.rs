6e86d9c10e1d4a7bfc65ecd22d061a8d0920b396
Alex Paldy <paldys@yahoo.com>
2025-12-21 11:13:25 +0000

    Solve Puzzle 7 part 2

--- 2025_rust/src/puzzles/day_07.rs
+++ 2025_rust/src/puzzles/day_07.rs
@@ -1,4 +1,5 @@
 use super::Result;
+use std::collections::HashMap;
 
 #[derive(PartialEq)]
 enum GridValue {
@@ -35,6 +36,44 @@ pub fn solve_first(input: String) -> Result {
         }
     }
     Result::Number(splits)
+}
+
+pub fn solve_second(input: String) -> Result {
+    let grid = parse_input(&input);
+    let mut cache_timelines: HashMap<(usize, usize), u64> = HashMap::new();
+    let start = grid[0].iter().position(|v| *v == GridValue::Beam).unwrap();
+    let timelines = get_timelines((start, 0_usize), &grid, &mut cache_timelines);
+    Result::Number(timelines)
+}
+
+fn get_timelines(
+    (x, y): (usize, usize),
+    grid: &Vec<Vec<GridValue>>,
+    cache_timelines: &mut HashMap<(usize, usize), u64>,
+) -> u64 {
+    if let Some(timelines) = cache_timelines.get(&(x, y)) {
+        *timelines
+    } else {
+        let timelines = if y + 1 < grid.len() {
+            match grid[y + 1][x] {
+                GridValue::Splitter => {
+                    let mut split_timelines = 0;
+                    if x > 0 {
+                        split_timelines += get_timelines((x - 1, y + 1), grid, cache_timelines);
+                    }
+                    if x + 1 < grid[0].len() {
+                        split_timelines += get_timelines((x + 1, y + 1), grid, cache_timelines);
+                    }
+                    split_timelines
+                }
+                _ => get_timelines((x, y + 1), grid, cache_timelines),
+            }
+        } else {
+            1
+        };
+        cache_timelines.insert((x, y), timelines);
+        timelines
+    }
 }
 
 fn parse_input(input: &str) -> Vec<Vec<GridValue>> {
@@ -80,5 +119,10 @@ mod tests {
     #[test]
     fn solves_first() {
         assert_eq_number(21, solve_first(String::from(RAW_INPUT)));
+    }
+
+    #[test]
+    fn solves_second() {
+        assert_eq_number(40, solve_second(String::from(RAW_INPUT)));
     }
 }
